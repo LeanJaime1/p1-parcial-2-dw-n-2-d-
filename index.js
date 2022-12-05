11b1b1c3a380a1a8a224a655b81781a0a9f7bd69
LeanJaime1 <102166092+LeanJaime1@users.noreply.github.com>
2022-12-05 18:54:12 -0300

    Array con productos y atributos

--- index.js
+++ index.js
@@ -4,3 +4,59 @@
  *  JAIME
  */
 
+let productos = [
+    {
+        id: 1,
+        nombre: 'Assasins Creed II PS3',
+        descripcion: 'Juego de Assasins Creed II en su versión para Playstation 3',
+        precio: 4500,
+        imagen: 'assasins-creed-ps3.jpg',
+        categoría: 'PS3',
+        items : 1,
+    },
+    {
+        id: 2,
+        nombre: 'God of War PS3',
+        descripcion: 'Juego de God of War en su versión para Playstation 3',
+        precio: 3500,
+        imagen: 'god-of-war-ps3.jpg',
+        categoría: 'PS3',
+        items : 1,
+    },
+    {
+        id: 3,
+        nombre: 'Destiny PS4',
+        descripcion: 'Juego de Destiny en su versión para Playstation 4',
+        precio: 6500,
+        imagen: 'destiny-ps4.jpg',
+        categoría: 'PS4',
+        items : 1,
+    },
+    {
+        id: 4,
+        nombre: 'Resident Evil 2 PS4',
+        descripcion: 'Juego de Resident Evil 2 en su versión para Playstation 4',
+        precio: 7500,
+        imagen: 'resident-evil-4-ps4.jpg',
+        categoría: 'PS4',
+        items : 1,
+    },
+    {
+        id: 5,
+        nombre: 'Injustice 2 PS5',
+        descripcion: 'Juego de Injustice 2 en su versión para Playstation 5',
+        precio: 10500,
+        imagen: 'injustice-2-ps5.jpg',
+        categoría: 'PS5',
+        items : 1,
+    },
+    {
+        id: 6,
+        nombre: 'GTA V PS5',
+        descripcion: 'Juego de GTA V en su versión para Playstation 5',
+        precio: 12500,
+        imagen: 'gta-v-ps5.jpg',
+        categoría: 'PS5',
+        items : 1,
+    },
+];
